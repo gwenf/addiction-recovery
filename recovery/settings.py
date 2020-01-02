c03b8304693e73803f30ff2397a6d09949c417ea
gwenf <gwenfaraday@gmail.com>
2020-01-02 12:53:22 -0500

    added basic page setup and templates

--- recovery/settings.py
+++ recovery/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'recovery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC__ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
+]
